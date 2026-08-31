45fb6677616a64e3089ab3d740c0eda4424987b1
DeepthiAR <deepthi.raghvendra@gmail.com>
2026-08-31 13:18:44 +0530

    ci: add GitHub Pages deploy workflow + set vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Advithiya-website/',
   server: {
     port: 3000,
     host: '127.0.0.1'
   }
 })
+
